7e8373f9c0680300fd8b150ef2fca8ee60bf695d
anton <antonkats@gmail.com>
2017-10-04 17:05:51 -0400

    display of offer creation is done

--- src/pages/NewDonationPage.js
+++ src/pages/NewDonationPage.js
@@ -1,22 +1,58 @@
-import React from "react"
+import React, {Component} from 'react';
+import { withStyles } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
 import { AppRegistry, ScrollView, Image, Text, View } from 'react-native';
 import ContentTemplate from '../templates/ContentTemplate'
+import Button from 'material-ui/Button';
 
-const NewDonationPage = (props) => {
+const styles = theme => ({
+  button: {
+    width: "5em",
+    alignSelf: "center"
+  },
+});
 
-  return (
-    <ContentTemplate title="Posting a donation">
-      <Text>donation page</Text>
-      <TextField
-        label="With placeholder multiline"
-        placeholder="Placeholder"
-        multiline
-        margin="normal"
-        helperText="Please select your currency"
-      />
-    </ContentTemplate>
-  );
+class NewDonationPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: "",
+      charsMax: 150,
+      charsLeft: 150
+    }
+
+    this.onTextChange = this.onTextChange.bind(this)
+  }
+
+  onTextChange(event) {
+    let description = event.target.value
+    let charsLeft = this.state.charsMax - description.length
+    if (charsLeft >= 0) {
+      this.setState({description: description, charsLeft: charsLeft})
+    }
+  }
+
+  helperText(chars) {
+    return "You have " + chars + " characters left to type"
+  }
+
+  render() {
+    let classes = this.props.classes;
+    return (
+      <ContentTemplate title="new offer">
+        <TextField
+          autoFocus={true}
+          onChange={this.onTextChange}
+          label={this.helperText(this.state.charsLeft)}
+          placeholder="What are you offering? Be concise."
+          multiline
+          margin="normal"
+          value={this.state.description}
+        />
+        <Button color="primary" className={classes.button} raised dense>Next</Button>
+      </ContentTemplate>
+    );
+  }
 };
 
-export default NewDonationPage
+export default withStyles(styles)(NewDonationPage)
--- src/templates/ContentTemplate.js
+++ src/templates/ContentTemplate.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import { AppRegistry, ScrollView, View, Image, Text } from 'react-native';
 
 export default function ContentTemplate(props) {
   return (
     <View>
-      <AppBar title={props.title}/>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       <ScrollView id="content">
         {props.children}
       </ScrollView>
