72abe14985e3e198caf1f370f31cf4099cbad6f6
anton <antonkats@gmail.com>
2017-10-24 08:34:43 -0400

    highest current bid and sending bid to server

--- src/pages/donation/BidDash.js
+++ src/pages/donation/BidDash.js
@@ -18,13 +18,19 @@ class Dash extends Component {
     super(props);
     this.state = {
       expanded: true,
-      highestBid: 0
+      highestBid: null,
+      hba: 0,
+      hbb: null,
+      minBid: 1
     };
 
     ServerComms.getHighestBid(props.id).then(b => {
-      console.log("highest bid", b)
-      // this.state.highestBid = Number(hb)
-      // this.state.minBid = this.state.highestBid + 1
+      console.log("highest bid", b);
+      if (b) {
+        let hba = b.bid.amount;
+        let hbb = b.info.name;
+        this.setState({highestBid: b, hba: hba, hbb: hbb, minBid: hba + 1})
+      }
     });
 
     this.onBidPress = this.onBidPress.bind(this)
@@ -37,30 +43,59 @@ class Dash extends Component {
   render() {
     return (
       <View className={this.props.classes.view}>
-        {this.state.expanded && <Panel highestBid={this.state.highestBid}/>}
+        {this.state.expanded && <Panel highestBidAmount={this.state.hba}
+                                       highestBidBy={this.state.hbb}
+                                       minBid={this.state.minBid}
+                                       id={this.props.id}/>}
         {!this.state.expanded && <BidButton onPress={this.onBidPress}/>}
       </View>
     )
   }
 }
 
-function PanelComp(props) {
-  const onBidPress = () => {
-  };
-  let minBid = props.highestBid + 1 + "";
+class PanelComp extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Paper className={props.classes.outerBidPanel}>
-      <View className={props.classes.innerBidPanel}>
-        <Typography>You have 0 {Tenge}hanks</Typography>
-        <Typography>Highest bid is {props.highestBid} by </Typography>
-        <TextField type='number' value={undefined} defaultValue={minBid} helperText="no decimals" label={"how" +
-        " many " + Tenge + "hanks would you" +
-        " like to bid?"}/>
-        <BidButton onPress={onBidPress}/>
-      </View>
-    </Paper>
-  )
+    this.state = {
+      userBid: null
+    };
+
+    this.onBidPress = this.onBidPress.bind(this);
+    this.onBidChange = this.onBidChange.bind(this)
+  }
+
+  onBidPress() {
+    ServerComms.sendBidToServer(this.props.id, this.state.userBid)
+  };
+
+  onBidChange(e) {
+    let newBid = e.target.value();
+    if (this.verifyBid(newBid)) {
+      this.setState({userBid: newBid})
+    }
+  }
+
+  verifyBid(value) {
+    return true
+  }
+
+  render() {
+    return (
+      <Paper className={this.props.classes.outerBidPanel}>
+        <View className={this.props.classes.innerBidPanel}>
+          <Typography>You have 0 {Tenge}hanks</Typography>
+          {this.props.highestBidBy &&
+          <Typography>Highest bid is {this.props.highestBidAmount} by {this.props.highestBidBy} </Typography>}
+          <TextField type='number' value={this.state.userBid ? this.state.userBid : this.props.minBid}
+                     helperText="no decimals" label={"how" +
+          " many " + Tenge + "hanks would you" +
+          " like to bid?"}/>
+          <BidButton onPress={this.onBidPress} onChange={this.onBidChange}/>
+        </View>
+      </Paper>
+    )
+  }
 }
 
 const Panel = withStyles(donationStyles)(PanelComp);
